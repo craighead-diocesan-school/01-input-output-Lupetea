863e7f7ff8ecd4645689d2e4252f9e367b9161f8
Lupetea <124636830+Lupetea@users.noreply.github.com>
2023-06-23 10:33:59 +1200

    I have finished the non maori mihi

--- code/3-non-maori-mihi.js
+++ code/3-non-maori-mihi.js
@@ -3,29 +3,33 @@
 // ####################################
 
 function nonMaoriMihi() {
-  alert('Non-Māori Mihi')
-  alert('This program will help you build a basic mihi.')
+  alert("Non-Māori Mihi")
+  alert("This program will help you build a basic mihi.")
   alert("First, you'll be asked a series of questions, then your mihi will be ready.")
 
-  alert('What is your name?')
+  alert("What is your name?")
   let name = prompt()
 
   // more questions and answers here
-  alert('What is your mountain?')
-  let mountain= prompt()
-  alert('What is your body of water?')
-  let river= prompt()
-  alert('What is your hometown?')
-  let hometown= prompt ()
+  let username = prompt("What is your name?")
+  alert("What is your mountain?")
+  let mountain = prompt()
+  alert("What is your body of water?")
+  let river = prompt()
+  alert("What is your hometown?")
+  let hometown = prompt()
 
-  alert('Your mihi:')
+  alert("Your mihi:")
   // more outputs here
-
-  alert('Ko ' + name + ' tōku ingoa,')
-  alert('Tēnā koutou,')
-  alert('Tēnā koutou,')
-  alert('Tēnā koutou, katoa.')
-do {
-    
-} while (condition);
+  alert("Tena koutou,")
+  alert("Kei te mihi au ki a" + mountain)
+  alert("Kei te mihi au ki a" + river)
+  alert("Ko Te Tiriti o Waitangi te waka,")
+  alert("Ko Aotearoa te iwi,")
+  alert("No" + hometown + "au,")
+  alert("Ko " + name + " tōku ingoa,")
+  alert("Tēnā koutou,")
+  alert("Tēnā koutou,")
+  alert("Tēnā koutou, katoa.")
+ 
 }
