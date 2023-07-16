c5b6cf5bef5822a58b8e193cd40852cb81a655d8
Lupetea <124636830+Lupetea@users.noreply.github.com>
2023-07-17 09:52:42 +1200

    improved welcome, I cancelled the uneccessary alerts

--- code/1-welcome.js
+++ code/1-welcome.js
@@ -7,23 +7,22 @@ function welcome() {
 
   alert('We want to know if you like programming!')
 
-  alert('Do you like programming ' + username + '?')
-  let answer = prompt()
+  let answer = prompt('Do you like programming'+username+'?')
 
-  alert('Great, you said ' + answer + 'that is awesome!')
+  alert('Great, you said ' + answer + '!')
+  alert("Let's learn some code today.")
   
 }
 
 function questionnaire() {
   let name= prompt("What's your name?")
-  
-  alert ("Nice, to meet you!")
 
   let breakfast= prompt ("What did you have for breakfast?")
 
-  alert("Sounds Delicious!")
-
   let colour= prompt ("What is your favourite colour at the moment?")
+  let age=prompt('How old are you?')
+  age=age+11
+  alert("you are"+age+ "years old on your next birthday")
 
   alert("I don't like that colour!")
 }
